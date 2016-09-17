6b298d891abcb4d0973689db218f28bc60de8586
IsaacNez <shak04995@gmail.com>
2016-09-17 05:27:03 -0600

    Se agregan los controllers

---
+++ Core/Controllers/ProductController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApplication1.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Http.Results;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public static IList<Product> prolist = new List<Product>();
+        [AcceptVerbs("GET")]
+        public Product RPCStyleMethodFetchFirstEmployees()
+        {
+            return prolist.FirstOrDefault();
+        }
+        static private string GetConnectionString()
+        {
+            return @"Data Source=ISAAC\ISAACSERVER;Initial Catalog=EPATEC;"
+                + "Integrated Security=true;";
+        }
+
+        [HttpGet]
+        [ActionName("Update")]
+        public void UpdateRecords(string attr, string avalue, string clause, string id)
+        {
+            string[] uattr = attr.Split(',');
+            string[] uvalue = avalue.Split(',');
+            string[] cattr = clause.Split(',');
+            string[] cvalue = id.Split(',');
+            string action = "";
+            CategoryController updateString = new CategoryController();
+            action = updateString.UpdateConnectionString("UPDATE PRODUCT ", uattr, uvalue, cattr, cvalue);
+            System.Diagnostics.Debug.WriteLine(action);
+
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = GetConnectionString();
+            System.Diagnostics.Debug.WriteLine("cargo base");
+            SqlCommand sqlCmd = new SqlCommand();
+            System.Diagnostics.Debug.WriteLine("cargo sqlcommand");
+
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = action;
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+        }
+
+        [HttpGet]
+        [ActionName("Get")]
+        public JsonResult<List<Product>> Get(string attribute, string id)
+        {
+            string[] attr = attribute.Split(',');
+            string[] ids = id.Split(',');
+            List<Product> values = new List<Product>();
+            Product emp = null;
+
+            System.Diagnostics.Debug.WriteLine("entrando al get");
+            SqlDataReader reader = null;
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = GetConnectionString();
+            System.Diagnostics.Debug.WriteLine("cargo base");
+            SqlCommand sqlCmd = new SqlCommand();
+            System.Diagnostics.Debug.WriteLine("cargo sqlcommand");
+            string action = "";
+
+            if(id != "undefined")
+            {
+                SucursalController constructor = new SucursalController();
+                action = constructor.FormConnectionString("PRODUCT", attr, ids);
+            }
+            else
+            {
+                action = "SELECT * FROM PRODUCT;";
+            }
+
+            System.Diagnostics.Debug.WriteLine(action);
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = action;
+            System.Diagnostics.Debug.WriteLine("cargo comando");
+
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            System.Diagnostics.Debug.WriteLine("estado " + myConnection.State);
+
+            reader = sqlCmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                emp = new Product();
+                emp.PR_ID = Convert.ToInt32(reader.GetValue(0).ToString());
+                emp.PName = reader.GetValue(1).ToString();
+                emp.Price = Convert.ToInt32(reader.GetValue(5).ToString());
+                emp.Extent = Convert.ToBoolean(reader.GetValue(2).ToString());
+                emp.PDescription = reader.GetValue(3).ToString();
+                emp.Quantity = Convert.ToInt32(reader.GetValue(4).ToString());
+                values.Add(emp);
+
+            }
+            
+            myConnection.Close();
+            return Json(values);
+        }
+        [HttpGet]
+        [ActionName("Delete")]
+        public void Delete(string attribute, string id)
+        {
+            string[] actions = attribute.Split(',');
+            string[] ids = id.Split(',');
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = GetConnectionString();
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            SucursalController deleteString = new SucursalController();
+            sqlCmd.CommandText = deleteString.FormConnectionString("DELETE FROM PRODUCT WHERE ", actions, ids);
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            int rowDeleted = sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+        }
+        [HttpPost]
+        [ActionName("Post")]
+        public void AddProduct(Product product)
+        {
+            System.Diagnostics.Debug.WriteLine(product.PR_ID);
+            System.Diagnostics.Debug.WriteLine("entrando al post");
+
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = GetConnectionString();
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            System.Diagnostics.Debug.WriteLine(myConnection.State);
+
+            sqlCmd.CommandText = "INSERT INTO PRODUCT(PR_ID,Price,Extent,PDescription,Quantity,PName) Values(@PR_ID,@Price,@Extent,@PDescription,@Quantity,@PName)";
+            System.Diagnostics.Debug.WriteLine("generando comando");
+
+            sqlCmd.Connection = myConnection;
+            sqlCmd.Parameters.AddWithValue("@PR_ID", product.PR_ID);
+            sqlCmd.Parameters.AddWithValue("@Price", product.Price);
+            sqlCmd.Parameters.AddWithValue("@Extent", product.Extent);
+            sqlCmd.Parameters.AddWithValue("@PDescription", product.PDescription);
+            sqlCmd.Parameters.AddWithValue("@Quantity", product.Quantity);
+            sqlCmd.Parameters.AddWithValue("@PName", product.PName);
+
+            myConnection.Open();
+            int rowInserted = sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+        }
+    }
+}
---
+++ Core/Models/Sucursal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Sucursal
+    {
+        public int S_ID { get; set; }
+        public string SName { get; set; }
+        public string SAddress { get; set; }
+
+    }
+}
