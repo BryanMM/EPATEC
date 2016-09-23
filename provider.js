5cabc9ff90ad87d89a315882fdb4991ef6818c9b
BryanMM <bryanmasis87@gmail.com>
2016-09-23 14:47:26 -0600

    Update provider.js
    added condition to get all providers

--- provider.js
+++ provider.js
@@ -88,13 +88,23 @@ angular.module('provider',[])
                 attributes+="Year";
                 values+=$scope.year;
             }
-                
-        console.log(attributes, values);
-        $http.get('http:///DESKTOP-E6QPTVT:7549/api/provider/get/'+attributes+"/"+values).
-        success(function (data, status, headers, config) {
-             $scope.Provider = data;
-        }).
-        error(function (data, status, headers, config) {
-            alert('Error retrieving providers information!')
-        }); }    
+        if(attributes==""){
+            console.log(attributes, values);
+            $http.get('http:///DESKTOP-E6QPTVT:7549/api/provider/get/none/undefined').
+            success(function (data, status, headers, config) {
+                 $scope.Provider = data;
+            }).
+            error(function (data, status, headers, config) {
+                alert('Error retrieving providers information!')
+            }); 
+        }
+        else{
+            console.log(attributes, values);
+            $http.get('http:///DESKTOP-E6QPTVT:7549/api/provider/get/'+attributes+"/"+values).
+            success(function (data, status, headers, config) {
+                 $scope.Provider = data;
+            }).
+            error(function (data, status, headers, config) {
+                alert('Error retrieving providers information!')
+            }); }    
 } ])
